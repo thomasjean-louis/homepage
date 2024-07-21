2da4e3e981e3f5c57bb84ca88bc7baa6b030557a
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-07-21 10:49:01 +0200

    init react commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+import GameServer from "./GameServer";
+
+function App() {
+  return (
+    <div className="App">
+      <GameServer />
+    </div>
+  );
+}
+
+export default App;
