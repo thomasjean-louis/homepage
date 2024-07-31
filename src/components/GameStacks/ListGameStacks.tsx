ae170a4a34f2b4caa039d2572ca840addedcc3b8
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-07-31 10:51:42 +0200

    init gamestacks api call

--- src/components/GameStacks/ListGameStacks.tsx
+++ src/components/GameStacks/ListGameStacks.tsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+
 function ListGameStacks() {
+  const [gamestacks, setGameStacks] = useState([]);
+
+  async function fechGameStacks() {
+    const gameStacksGetEndpoint =
+      import.meta.env.VITE_LOAD_BALANCER_HTTPS_URL + "/gamestacks";
+
+    try {
+      const response = await axios.get(gameStacksGetEndpoint);
+      setGameStacks(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fechGameStacks();
+  }, []);
+
   return <div>ListGameStacks</div>;
 }
 
